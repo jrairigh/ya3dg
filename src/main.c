107565aa275cff2db36caf825ce564fd7458ae1a
Josh Rairigh <Josh.Rairigh@aristocrat.com>
2026-05-27 16:56:05 -0500

    Render to image then update to texture

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#include <stdio.h>
-
 inline Vector4 Vector4Transform(Vector4 v, Matrix mat)
 {
     return QuaternionTransform(v, mat);
@@ -16,14 +14,14 @@ Vector3 cube[] = {
     { 1.0f, -1.0f, -4.0f},
     
     // front face
-    {-1.0f, -1.0f,  -1.0f},
-    {-1.0f,  1.0f,  -1.0f},
-    { 1.0f,  1.0f,  -1.0f},
-    { 1.0f, -1.0f,  -1.0f},
+    {-1.0f, -1.0f,  -2.0f},
+    {-1.0f,  1.0f,  -2.0f},
+    { 1.0f,  1.0f,  -2.0f},
+    { 1.0f, -1.0f,  -2.0f},
 };
 
 int indices[] = {
-    0,1,2,3,0,4,5,6,7,4
+    0,1,2,3,0,4,5,6,7,4,5,1,2,6,7,3
 };
 
 typedef struct _MyCamera
@@ -40,11 +38,13 @@ typedef struct _MyCamera
 MyCamera g_camera;
 int g_screen_width = 800;
 int g_screen_height = 600;
+Image g_image;
+Texture2D g_render_target;
 
 void SetupCamera(MyCamera* camera);
 Matrix GetTransform(const MyCamera* camera);
 Matrix MatrixViewport();
-Vector3 GetCubeTransformedPoint(Vector4 v, Matrix mat);
+Vector3 GetCubeTransformedPoint(Vector3 v, Matrix mat);
 
 int main(void) 
 {
@@ -53,26 +53,47 @@ int main(void)
 
     SetupCamera(&g_camera);
 
-    const Matrix transform = GetTransform(&g_camera);
-
+    g_image = GenImageColor(g_screen_width, g_screen_height, BLACK);
+    g_render_target = LoadTextureFromImage(g_image);
+    
     while (!WindowShouldClose())
     {
-        BeginDrawing();
+        const float frame_time = (float)GetTime();
+        g_camera.eye.x = 10.0f*cosf(frame_time);
+        g_camera.eye.y = 0.0f;
+        g_camera.eye.z = 10.0f*sinf(frame_time);
+        const Matrix transform = GetTransform(&g_camera);
 
+        ImageClearBackground(&g_image, BLACK);
+        
         int size = sizeof(indices) / sizeof(indices[0]);
         for(int i = 0; i < size; ++i)
         {
-            const Vector3 p1 = GetCubeTransformedPoint((Vector4){ cube[indices[i]].x, cube[indices[i]].y, cube[indices[i]].z, 1.0f }, transform);
-            const Vector3 p2 = GetCubeTransformedPoint((Vector4){ cube[indices[(i + 1) % size]].x, cube[indices[(i + 1) % size]].y, cube[indices[(i + 1) % size]].z, 1.0f }, transform);
-
+            const Vector3 p1 = GetCubeTransformedPoint(cube[indices[i]], transform);
+            const Vector3 p2 = GetCubeTransformedPoint(cube[indices[(i + 1) % size]], transform);
+            
             Vector2 start = { p1.x, p1.y };
             Vector2 end = { p2.x, p2.y };
-            DrawLineV(start, end, GREEN);
+            ImageDrawLineV(&g_image, start, end, GREEN);
         }
 
+        UpdateTexture(g_render_target, g_image.data);
+        
+        BeginDrawing();
         ClearBackground(BLACK);
+        DrawTextureV(g_render_target, (Vector2){ 0, 0 }, WHITE);
         DrawFPS(10, 10);
         EndDrawing();
+    }
+
+    if(IsTextureValid(g_render_target))
+    {
+        UnloadTexture(g_render_target);
+    }
+
+    if(IsImageValid(g_image))
+    {
+        UnloadImage(g_image);
     }
 
     CloseWindow();
@@ -112,12 +133,12 @@ Matrix MatrixViewport(int screen_width, int screen_height)
     return result;
 }
 
-Vector3 GetCubeTransformedPoint(Vector4 v, Matrix mat)
+Vector3 GetCubeTransformedPoint(Vector3 v, Matrix mat)
 {
+    const Vector4 v2 = (Vector4){ v.x, v.y, v.z, 1.0f };
     const Matrix viewport = MatrixViewport(g_screen_width, g_screen_height);
-    Vector4 transformed = Vector4Transform(v, mat);
+    Vector4 transformed = Vector4Transform(v2, mat);
     transformed = Vector4Scale(transformed, 1.0f / transformed.w);
-    //DrawText(TextFormat("Vertex %d: (%.2f, %.2f, %.2f, %.2f)", i, transformed.x, transformed.y, transformed.z, transformed.w), 10, 30 + 20*i, 20, RAYWHITE);
     const Vector4 p2 = Vector4Transform(transformed, viewport);
     const Vector3 p = { p2.x, p2.y, p2.z };
     return p;
